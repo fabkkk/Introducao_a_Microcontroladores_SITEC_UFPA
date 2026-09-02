001e51e10f0278858fc201d0ca3d6fc7776c330d
Fábio Ribeiro Filho <fabkk13@gmail.com>
2026-09-02 14:38:50 -0300

    Sensor de Movimento - Código

--- Códigos/SensorDeMovimento.ino
+++ Códigos/SensorDeMovimento.ino
@@ -1,6 +1,8 @@
+// primeiramente definimos os pinos para o LED e para o sensor
 const int PIR = 23;
 const int LED = 21;
 
+// definindo as funções de cada pino
 void setup()
 {
     pinMode(PIR, INPUT);
@@ -9,6 +11,7 @@ void setup()
 
 void loop()
 {
+    // criamos uma variável para armazenar a leitura do sensor
     int movimento = digitalRead(PIR);
 
     if (movimento == HIGH)
